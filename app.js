ca74d67c9b1582d33b4919cc182a7d2702a3a3c5
Yoav Bergner <ybergner@gmail.com>
2023-04-10 21:38:02 -0400

    PWA

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const resetButton = document.getElementById("reset");
 let countdown;
 let remainingTime;
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("sw.js").then(
+      (registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      },
+      (error) => {
+        console.log("Service Worker registration failed:", error);
+      }
+    );
+  });
+}
+
 function formatTime(time) {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("session-clock-cache").then((cache) => {
+      return cache.addAll(["index.html", "app.js", "style.css"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
