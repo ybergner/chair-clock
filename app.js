bc529eee2963e490e06ebcba4d0e9d7c24bbe567
Yoav Bergner <ybergner@gmail.com>
2023-04-10 21:20:38 -0400

    fullscreen button

--- app.js
+++ app.js
@@ -60,6 +60,33 @@ function updateClockColor() {
   }
 }
 
+function toggleFullScreen() {
+  const doc = window.document;
+  const docEl = doc.documentElement;
+
+  const requestFullScreen =
+    docEl.requestFullscreen ||
+    docEl.mozRequestFullScreen ||
+    docEl.webkitRequestFullScreen ||
+    docEl.msRequestFullscreen;
+  const cancelFullScreen =
+    doc.exitFullscreen ||
+    doc.mozCancelFullScreen ||
+    doc.webkitExitFullscreen ||
+    doc.msExitFullscreen;
+
+  if (
+    !doc.fullscreenElement &&
+    !doc.mozFullScreenElement &&
+    !doc.webkitFullscreenElement &&
+    !doc.msFullscreenElement
+  ) {
+    requestFullScreen.call(docEl);
+  } else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 startButton.addEventListener("click", startCountdown);
 stopButton.addEventListener("click", stopCountdown);
 resetButton.addEventListener("click", resetCountdown);
